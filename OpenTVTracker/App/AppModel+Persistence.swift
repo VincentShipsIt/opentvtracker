5ceaff204b8256979cb70ef0ea566e919f0ab9e9
VincentShipsIt <vincent@shipshit.dev>
2026-07-21 12:33:10 +0200

    refactor(#35): separate persistence merging

--- OpenTVTracker/App/AppModel+Persistence.swift
+++ OpenTVTracker/App/AppModel+Persistence.swift
@@ -1,6 +1,28 @@
 import Foundation
 
 extension AppModel {
+    func merging(savedTitles: [MediaTitle], catalogTitles: [MediaTitle]) -> [MediaTitle] {
+        let savedByID = Dictionary(uniqueKeysWithValues: savedTitles.map { ($0.id, $0) })
+        let catalogIDs = Set(catalogTitles.map(\.id))
+        let refreshedCatalog = catalogTitles.map { catalogTitle in
+            guard let savedTitle = savedByID[catalogTitle.id] else { return catalogTitle }
+            var refreshedTitle = catalogTitle
+            refreshedTitle.state = savedTitle.state
+            refreshedTitle.progress = savedTitle.progress
+            refreshedTitle.userRating = savedTitle.userRating
+            refreshedTitle.notes = savedTitle.notes
+            refreshedTitle.rewatchCount = savedTitle.rewatchCount
+            refreshedTitle.lastWatchedAt = savedTitle.lastWatchedAt
+            refreshedTitle.isDismissed = savedTitle.isDismissed
+            refreshedTitle.isDisliked = savedTitle.isDisliked
+            refreshedTitle.personalWatchlist = savedTitle.personalWatchlist
+            refreshedTitle.watchedEpisodeIDs = savedTitle.watchedEpisodeIDs
+            return refreshedTitle
+        }
+        let localOnlyTitles = savedTitles.filter { !catalogIDs.contains($0.id) }
+        return refreshedCatalog + localOnlyTitles
+    }
+
     func persist() {
         let snapshot = self.snapshot
         let store = store
